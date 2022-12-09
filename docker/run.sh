10cc1633d99ed12e9d77a87a1d33a9046e69c390
odieagnes24 <odieagnes@gmail.com>
2022-12-09 19:14:27 +0800

    docker configs

---
+++ docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd /var/www
+
+php artisan migrate:fresh
+# php artisan migrate:fresh --seed
+# php artisan cache:clear
+# php artisan route:cache
+
+# /usr/bin/supervisord -c /etc/supervisord.conf
